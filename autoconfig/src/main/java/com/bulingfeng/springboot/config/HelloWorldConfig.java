2f960beb8bc30fbcc6c772cab7589eaf08d5af37
bulingfeng <413957294@qq.com>
2022-07-13 09:47:53 +0800

    使用接口的方式来实现enable

--- autoconfig/src/main/java/com/bulingfeng/springboot/config/HelloWorldConfig.java
+++ autoconfig/src/main/java/com/bulingfeng/springboot/config/HelloWorldConfig.java
@@ -3,7 +3,7 @@ package com.bulingfeng.springboot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+
 public class HelloWorldConfig {
 
     @Bean

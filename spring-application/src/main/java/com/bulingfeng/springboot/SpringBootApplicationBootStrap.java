ee679b97e461f305937d7d6537e6d29d943e3d76
bulingfeng <413957294@qq.com>
2022-07-19 10:14:32 +0800

    spring-application

---
+++ spring-application/src/main/java/com/bulingfeng/springboot/SpringBootApplicationBootStrap.java
@@ -0,0 +1,24 @@
+package com.bulingfeng.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootApplication
+public class SpringBootApplicationBootStrap {
+    public static void main(String[] args) {
+        // 第一种启动web的形式
+//        SpringApplication.run(SpringBootApplicationBootStrap.class,args);
+
+        // 第二章启动web的形式
+        Set<String> sources=new HashSet<>();
+        sources.add(SpringBootApplicationBootStrap.class.getName());
+        SpringApplication application=new SpringApplication();
+        // 只要把含有@SpringBootApplication的类名放到sources即可
+        application.setSources(sources);
+        application.run(args);
+    }
+}
